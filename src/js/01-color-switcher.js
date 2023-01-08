c26e6dcdb8ca88e89fd10f2b2715c0f7ae7a711f
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-01-08 14:55:25 +0200

    1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+const isActive = true;
+
+startBtn.addEventListener('click', () => {
+  intervalId = setInterval(() => {
+    const color = getRandomHexColor();
+    document.body.style.backgroundColor = color;
+  }, 1000);
+  startBtn.setAttribute('disabled', 'true');
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(intervalId);
+  startBtn.removeAttribute('disabled');
+});
